ccd2ab14aaa80c0f3400b16ff706049bb2ca72f6
Omorval3008 <omorval3008@g.educaand.es>
2023-12-18 21:29:47 +0100

    ej 2 implementado

--- src/main/java/daw/EJ1.java
+++ src/main/java/daw/EJ1.java
@@ -4,7 +4,9 @@
 package daw;
 
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -36,24 +38,93 @@ public class EJ1 {
         int arrayElegido = random.nextInt(1, 3);
         if (arrayElegido == 1) {
             int posAleatoria = random.nextInt(0, arrayDoubles.length);
-            arrayDoubles[posAleatoria]=6.3;
+            arrayDoubles[posAleatoria] = 6.3;
         } else {
             int posAleatoria = random.nextInt(0, copiaArrayDoubles.length);
             copiaArrayDoubles[posAleatoria] = 6.3;
         }
         /*muestro ambos por consola*/
         System.out.println(Arrays.toString(arrayDoubles));
-        System.out.println("---------------------------------------");
+        System.out.println("----------------------------------------");
         System.out.println(Arrays.toString(copiaArrayDoubles));
         System.out.println("----------------------------------------");
         /*Comparo los arrays y devuelvo si son iguales*/
         arraysIguales = Arrays.equals(arrayDoubles, copiaArrayDoubles);
         System.out.println(arraysIguales);
-    }
-    public static int[] rellenaArray(int[] array){
-        Random random = new Random();
-        for(int i : array){
-            array[i]=random.nextInt(10, 101);
+        System.out.println("----------------------------------------");
+        /*Ej 2*/
+ /*genero un array con el metodo rellenaArray*/
+        int[] arrayDeNumAleatorios = new int[20];
+        rellenaArray(arrayDeNumAleatorios);
+        System.out.println("----------------------------------------");
+        System.out.println(Arrays.toString(arrayDeNumAleatorios));
+        /*Ordeno las posiciones de mi array*/
+        Arrays.sort(arrayDeNumAleatorios);
+        System.out.println(Arrays.toString(arrayDeNumAleatorios));
+        System.out.println("----------------------------------------");
+        /*busco un numero especifico*/
+        Scanner teclado = new Scanner(System.in);
+        boolean errorNum = true;
+        int numEspecifico = 0;
+        do {
+            try {
+                System.out.println("Introduce un número entero");
+                numEspecifico = teclado.nextInt();
+                teclado.nextLine();/*limpio la variable en caso de que sea correcto*/
+                errorNum = false;
+            } catch (InputMismatchException ime) {
+                System.out.println("No has cumplido con los requisitos especificados");
+                errorNum = true;
+                teclado.nextLine();/*limpio la variable en caso de error*/
+            }
+        } while (errorNum);
+        /*encuentra la posición del número especificado*/
+        int pos = numSpecifico(arrayDeNumAleatorios, numEspecifico);
+        if (pos != 504) {/*504 por que es mi valor por defecto si no se encuentra la
+            posición específica*/
+            System.out.println(numEspecifico + " esta en la posición " + pos + " del array");
+        } else {
+            System.out.println(numEspecifico + " no se encuentra en el array");
         }
+
+    }
+
+    /*Ej 2*/
+    public static int[] rellenaArray(int[] array) {
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {/*rellena las posiciones de mi array*/
+            array[i] = random.nextInt(10, 101);
+        }
+        return array;
+    }
+
+    public static int numSpecifico(int[] array, int num) {
+        /*posiciones*/
+        int mitad = 0;
+        int izquierda = 0;
+        int derecha = array.length - 1;
+        /*le indico el numero que deseo buscar*/
+        int numABuscar = num;
+        boolean numEncontrado = false;
+        while ((izquierda <= derecha) && (!numEncontrado)) {
+            mitad = (izquierda + derecha) / 2;
+            if (array[mitad] == numABuscar) {
+                numEncontrado = true;/*si encuentra la posicion, indica que la ha encontrado*/
+            } else if (array[mitad] > numABuscar) {
+                derecha = mitad - 1;
+                /*buscar en el cuadrante izq del array*/
+            } else {
+                izquierda = mitad + 1;
+                /*buscar en el cuadrante derecho del array*/
+            }
+        }
+        /*si el número ha sido encontrado, devuelve la posición en la que esta*/
+ /*si no devuelve un valor por defecto, en este caso 504*/
+        if (numEncontrado) {
+            return mitad;
+        } else {
+            return 504;
+        }
+
     }
 }
