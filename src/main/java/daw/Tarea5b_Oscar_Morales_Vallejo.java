3700f5259c351bd15bbcd95891cadf3833542a16
Omorval3008 <omorval3008@g.educaand.es>
2023-12-18 09:46:00 +0100

    inicio proyecto

---
+++ src/main/java/daw/Tarea5b_Oscar_Morales_Vallejo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package daw;
+
+/**
+ *
+ * @author oscar
+ */
+public class Tarea5b_Oscar_Morales_Vallejo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
