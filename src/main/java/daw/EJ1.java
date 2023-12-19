aea276fb5cdf8858c1aa655f609dc2f5f374ca68
Omorval3008 <omorval3008@g.educaand.es>
2023-12-19 10:57:19 +0100

    Ejercicio corregido

--- src/main/java/daw/EJ1.java
+++ src/main/java/daw/EJ1.java
@@ -53,7 +53,7 @@ public class EJ1 {
         System.out.println(arraysIguales);
         System.out.println("----------------------------------------");
         /*Ej 2*/
- /*genero un array con el metodo rellenaArray*/
+        /*genero un array con el metodo rellenaArray*/
         int[] arrayDeNumAleatorios = new int[20];
         rellenaArray(arrayDeNumAleatorios);
         System.out.println("----------------------------------------");
@@ -79,9 +79,8 @@ public class EJ1 {
             }
         } while (errorNum);
         /*encuentra la posición del número especificado*/
-        int pos = numSpecifico(arrayDeNumAleatorios, numEspecifico);
-        if (pos != 504) {/*504 por que es mi valor por defecto si no se encuentra la
-            posición específica*/
+        int pos = Arrays.binarySearch(arrayDeNumAleatorios,numEspecifico);
+        if (pos >=0) {/*se ha decidido implementar con binary search*/
             System.out.println(numEspecifico + " esta en la posición " + pos + " del array");
         } else {
             System.out.println(numEspecifico + " no se encuentra en el array");
@@ -97,34 +96,35 @@ public class EJ1 {
         }
         return array;
     }
-
-    public static int numSpecifico(int[] array, int num) {
-        /*posiciones*/
-        int mitad = 0;
-        int izquierda = 0;
-        int derecha = array.length - 1;
-        /*le indico el numero que deseo buscar*/
-        int numABuscar = num;
-        boolean numEncontrado = false;
-        while ((izquierda <= derecha) && (!numEncontrado)) {
-            mitad = (izquierda + derecha) / 2;
-            if (array[mitad] == numABuscar) {
-                numEncontrado = true;/*si encuentra la posicion, indica que la ha encontrado*/
-            } else if (array[mitad] > numABuscar) {
-                derecha = mitad - 1;
-                /*buscar en el cuadrante izq del array*/
-            } else {
-                izquierda = mitad + 1;
-                /*buscar en el cuadrante derecho del array*/
-            }
-        }
+/*metodo que hace lo mismo que binarySearch*/
+//    public static int numSpecifico(int[] array, int num) {/* hace lo mismo que
+//        Arrays.binarySearch(array, num)*/
+//        /*posiciones*/
+//        int mitad = 0;
+//        int izquierda = 0;
+//        int derecha = array.length - 1;
+//        /*le indico el numero que deseo buscar*/
+//        int numABuscar = num;
+//        boolean numEncontrado = false;
+//        while ((izquierda <= derecha) && (!numEncontrado)) {
+//            mitad = (izquierda + derecha) / 2;
+//            if (array[mitad] == numABuscar) {
+//                numEncontrado = true;/*si encuentra la posicion, indica que la ha encontrado*/
+//            } else if (array[mitad] > numABuscar) {
+//                derecha = mitad - 1;
+//                /*buscar en el cuadrante izq del array*/
+//            } else {
+//                izquierda = mitad + 1;
+//                /*buscar en el cuadrante derecho del array*/
+//            }
+//        }
         /*si el número ha sido encontrado, devuelve la posición en la que esta*/
- /*si no devuelve un valor por defecto, en este caso 504*/
-        if (numEncontrado) {
-            return mitad;
-        } else {
-            return 504;
-        }
-
-    }
+        /*si no devuelve un valor por defecto, en este caso 504*/
+//        if (numEncontrado) {
+//            return mitad;
+//        } else {
+//            return 504;
+//        }
+//
+//    }
 }
